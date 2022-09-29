7a9b0c8236c5e64b993f62939812da3c33547f08
Alevtin_linux_home <antonov3017@gmail.com>
2022-09-29 19:50:54 +0300

    Add the solution of the task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+
+int string_length = 3;
+string[] result_array = new string[array.Length] ;
+int j=0;
+
+for(int i=0 ; i<array.Length; i++)
+   if(array[i].Length < string_length)
+    result_array[j] = array[i];
+    j+=1;
+
+result_array = result_array.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+Console.WriteLine("[{0}]", string.Join(", ", result_array));
